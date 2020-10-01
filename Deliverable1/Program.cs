94c00a2d61d060427c6dffd0003b3194fe65260c
alex <acrang@gmail.com>
2020-09-30 20:08:35 -0400

    First commit for Deliverable 1

---
+++ Deliverable1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Deliverable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a valid password. Your password must be between 7-12 characters, and contain the following: one uppercase letter, one lowercase letter, and an exclamation point (!)");
+            string password = Console.ReadLine();
+            int length = password.Length;
+            if (length >= 7
+                && length <= 12
+                && password.Any(char.IsUpper)
+                && password.Any(char.IsLower)
+                && password.Contains("!"))
+            {
+                Console.WriteLine("Password valid and accepted");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
